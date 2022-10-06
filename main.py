f9ec85964542a929916e638e00f998ea8564113f
nedoletoff <nedoletoff@yandex.ru>
2022-10-06 12:49:26 +0300

    что-то

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-IDEAKEYSIZE = 16
-IDEABLOCKSIZE = 8
-IDEAROUNDS = 8
-IDEAKEYLEN = 6 * IDEAROUNDS + 4
+class IDEA(object):
+    IDEAKEYSIZE = 16
+    IDEABLOCKSIZE = 8
+    IDEAROUNDS = 8
+    IDEAKEYLEN = 6 * IDEAROUNDS + 4
 
-class IDEA:
-    print()
+    @staticmethod
+    def pr():
+        print(IDEA.IDEAKEYSIZE, " ",
+              IDEA.IDEAROUNDS, " ",
+              IDEA.IDEAKEYLEN, " ",
+              IDEA.IDEAKEYLEN)
 
 
-
-
+IDEA.pr()
+l = list()
+l.append("2")
+print(l)
