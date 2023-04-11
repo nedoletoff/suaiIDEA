a85bec9c6419fe659705b023abf0c3141ddc9206
nedoletoff <nedoletoff@yandex.ru>
2023-04-11 22:53:48 +0300

    сделал доп с случайной последовательностью и проверкой ее алгоритмом

--- lab5/main.py
+++ lab5/main.py
@@ -1,7 +1,9 @@
+import random
+
 import matplotlib.pyplot as plt
 
 
-def Berlekamp_Massey_algorithm(sequence):
+def Berlekamp_Massey_algorithm(sequence: list) -> (str, int):
     N = len(sequence)
     s = sequence[:]
 
@@ -51,48 +53,65 @@ def Berlekamp_Massey_algorithm(sequence):
     return print_poly(f), l
 
 
-def read_sequence() -> list:
+def read_sequence(filename: str) -> list:
     text = ''
-    with open('sequence.txt', 'r') as f:
+    with open(filename, "r") as f:
         for line in f:
             text += line
-    sequence = text.split(', ')
+    sequence = text.split(", ")
     sequence.pop(-1)
     sequence = [int(i) for i in sequence]
     return sequence
 
 
-def get_symbols(num, sequence):
+def get_symbols(num: int, sequence: list) -> list:
     symbols = sequence[:num]
     return symbols
 
 
-def draw_plot(linear_list: list, lenght_list: list):
+def draw_plot(linear_list: list, lenght_list: list, name="plot"):
     temp = [x / 2 for x in lenght_list]
     plt.plot(lenght_list, linear_list)
     plt.plot(lenght_list, temp, 'r')
     plt.xlabel('Length of sequence')
     plt.ylabel('Linear complexity')
-    plt.title('Linear complexity')
-    plt.savefig('plot.png')
+    plt.title(f'Linear complexity {name}')
+    plt.savefig(name + ".png")
     plt.show()
 
 
-def write_result(seq):
-    with open("result.txt", "w") as f:
+def write_result(seq: list, filename: str):
+    with open(filename, "w") as f:
         for i in range(1, len(seq) + 1):
             temp_poly, temp_linear = Berlekamp_Massey_algorithm(get_symbols(i, seq))
             f.write(f"Length: {i:3d}, \tLinear: {temp_linear:2d}, \tPolynomial: {temp_poly};\n")
 
 
-if __name__ == '__main__':
-    seq = read_sequence()
+def generate_file_with_random_sequence(len_of_sequence: int, filename: str):
+    with open(filename, "w") as f:
+        for i in range(len_of_sequence - 1):
+            f.write(str(random.randint(0, 1)))
+            f.write(", ")
+        f.write(str(random.randint(0, 1)))
+
+
+def get_linear(seq: list) -> list:
     linear_list = []
     for i in range(1, len(seq) + 1):
-        poly, linear = Berlekamp_Massey_algorithm(get_symbols(i, seq))
-        print(f"Length: {i}")
-        print(f"Polynomial: {poly}")
-        print(f"Linear: {linear}\n")
+        _, linear = Berlekamp_Massey_algorithm(get_symbols(i, seq))
         linear_list.append(linear)
-    write_result(seq)
-    draw_plot(linear_list, list(range(1, len(seq) + 1)))
+    return linear_list
+
+
+if __name__ == '__main__':
+    # generate_file_with_random_sequence(100, "random_seq.txt")
+    seq = read_sequence("random_seq.txt")
+    seq1 = read_sequence("sequence.txt")
+
+    linear_list = get_linear(seq)
+    linear_list1 = get_linear(seq1)
+
+    write_result(seq, "result_random_seq.txt")
+    write_result(seq1, "result_seq.txt")
+    draw_plot(linear_list, list(range(1, len(seq) + 1)), "random sequence")
+    draw_plot(linear_list1, list(range(1, len(seq1) + 1)), "generated sequence")
