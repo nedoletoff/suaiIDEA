82d43909b3981e5f226ca8e52ed642fabe9fae8d
nedoletoff <nedoletoff@yandex.ru>
2023-04-18 11:52:23 +0300

    добавил удаление ресурсов и людей, и программа теперь работает в консоли

--- lab6/main.py
+++ lab6/main.py
@@ -41,7 +41,7 @@ class Hierarchy:
             raise Exception(f'Cant remove user {user_name}, that user does not exist')
 
     def sort(self):
-        self.hierarchy.sort(key=lambda x: x.place)
+        self.hierarchy = dict(sorted(self.hierarchy.items()))
 
     def get_user_place(self, user_name: str) -> list:
         if user_name in self.hierarchy:
@@ -87,6 +87,13 @@ class ResourcesOwners:
         self.filename = filename
         self.hierarchy = hierarchy
 
+    def __str__(self):
+        res = ''
+        with open(self.filename, 'r') as f:
+            for line in f:
+                res += line.split('\t')[0] + '\n'
+        return res
+
     def get_resource_owners(self, resources_name: str) -> list:
         with open(self.filename, 'r') as f:
             for line in f:
@@ -103,77 +110,110 @@ class ResourcesOwners:
 
     def remove_resource(self, resource_name: str):
         with open(self.filename, 'r') as f:
-            for line in f:
-                if resource_name in line:
-                    f.write(line.replace(resource_name, ''))
+            lines = f.readlines()
+        with open(self.filename, 'w') as f:
+            for line in lines:
+                if resource_name not in line:
+                    f.write(line)
 
     def add_user_to_resource(self, user_name: str, resource_name: str):
-        with open(self.filename, 'a') as f:
-            f.write(resource_name + '\t' + user_name + ',\n')
-            self.hierarchy.generate_resources_rights(self.filename, self.filename)
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
+
+        with open(self.filename, 'w') as f:
+            for line in lines:
+                if resource_name in line:
+                    f.write(line.replace('\n', user_name + ',\n'))
+                else:
+                    f.write(line)
+        #self.hierarchy.generate_resources_rights(self.filename, self.filename)
 
     def remove_user_access(self, user_name: str, resource_name: str):
         with open(self.filename, 'r') as f:
-            for line in f:
+            lines = f.readlines()
+
+        with open(self.filename, 'w') as f:
+            for line in lines:
                 if user_name in line and resource_name in line:
                     f.write(line.replace(user_name + ',', ''))
+                else:
+                    f.write(line)
 
 
-def work_circle():
+def work_circle(filename_users: str, filename_resources: str):
+    hierarchy = Hierarchy(filename_users)
+    resources_owners = ResourcesOwners(filename_resources, hierarchy)
     exit_flag = False
     while not exit_flag:
         print('1. Add user')
         print('2. Remove user')
         print('3. Add resource')
         print('4. Remove resource')
-        print('5. Exit')
+        print('5. Get resource owners')
+        print('6. Get user resources')
+        print('7. Get all users')
+        print('8. Get all resources')
+        print('9. Exit')
         choice = input('Enter your choice: ')
         if choice == '1':
             user_name = input('Enter user name: ')
             place = input('Enter place: ').split(',')
             try:
                 hierarchy.add_user(user_name, place)
+                hierarchy.sort()
             except Exception as e:
                 print(e)
         elif choice == '2':
             user_name = input('Enter user name: ')
             try:
                 hierarchy.remove_user(user_name)
+                hierarchy.sort()
             except Exception as e:
                 print(e)
         elif choice == '3':
             resource_name = input('Enter resource name: ')
+            user_name = input('Enter user name: ')
             try:
-                hierarchy.add_user_to_resource(user_name, resource_name)
+                hierarchy.get_user_place(user_name)
+                resources_owners.add_user_to_resource(user_name, resource_name)
             except Exception as e:
                 print(e)
         elif choice == '4':
             resource_name = input('Enter resource name: ')
-
-
+            try:
+                resources_owners.remove_resource(resource_name)
+            except Exception as e:
+                print(e)
+        elif choice == '5':
+            resource_name = input('Enter resource name: ')
+            try:
+                print(resources_owners.get_resource_owners(resource_name))
+            except Exception as e:
+                print(e)
+        elif choice == '6':
+            user_name = input('Enter user name: ')
+            try:
+                print(resources_owners.get_user_resources(user_name))
+            except Exception as e:
+                print(e)
+        elif choice == '7':
+            try:
+                print(hierarchy.hierarchy)
+            except Exception as e:
+                print(e)
+        elif choice == '8':
+            try:
+                print(resources_owners)
+            except Exception as e:
+                print(e)
+        elif choice == '9':
+            exit_flag = True
+        else:
+            print('Wrong choice')
 
 
 if __name__ == '__main__':
     hierarchy = Hierarchy()
     hierarchy.init_from_file('hierarchy.txt')
-    hierarchy.generate_resources_rights('res1.txt')
-    '''
-    print(get_resource_owners('res1&rights.txt', 'John Smith'))
-    print(get_user_resources('res1&rights.txt', 'John Smith'))
-
-    try:
-        hierarchy.add_user('Lisa Yen', [0, 1, 4])
-        hierarchy.add_user('John Done', [0])
-    except Exception as e:
-        print(e)
-
-    print(hierarchy.hierarchy)
-
-    try:
-        hierarchy.remove_user('Lisa Yen')
-        hierarchy.remove_user('John Done')
-    except Exception as e:
-        print(e)
-
-    print(hierarchy.hierarchy)
- '''
+    hierarchy.generate_resources_rights('res1.txt', 'res1rights.txt')
+    work_circle('hierarchy.txt', 'res1rights.txt')
